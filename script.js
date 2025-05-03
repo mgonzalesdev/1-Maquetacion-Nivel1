1d3cfc6b94568d6df56fa646408a258ba881b16a
Mariela Gonzales <mariela.gonzales.segobia@gmail.com>
2025-05-03 05:25:40 -0400

    Maquetacion Html boostrap

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 const btnMenu = document.getElementById("btn-menu");
-btnMenu.addEventListener("click", function () {    
-    //document.getElementById("icon-menu").style.backgroundImage = "url('img/icon-close.svg')";
-    //let visible = btnMenu.getAttribute('aria-expanded') === 'true' ? 'false' : 'true';
-   
+btnMenu.addEventListener("click", function () {
+    updateMenu();
+});
+
+let menu = document.getElementById('navbarSupportedContent');
+menu.addEventListener("click", function () {
     let visible = btnMenu.getAttribute('aria-expanded');
     if (visible == "true") {
-        document.getElementById("logo").setAttribute("src", "img/logo-bookmark-white.svg");
-        document.getElementById("header").classList.add("menu-movil");
-        document.getElementById("icon-menu").classList.add("bg-icon");
-       
-    } else {
-        document.getElementById("logo").setAttribute("src", "img/logo-bookmark.svg");
-        document.getElementById("header").classList.remove('menu-movil');
-        document.getElementById("icon-menu").classList.remove("bg-icon");
+        updateMenu();
+        btnMenu.click();
     }
-
 });
+
+function updateMenu() {
+    let visible = btnMenu.getAttribute('aria-expanded');
+    let logo = document.getElementById("logo");
+    let header = document.getElementById("header");
+    let btnLogin = document.getElementById("btn-login");
+
+    if (visible == "true") {
+        logo.setAttribute("src", "img/logo-bookmark-white.svg");
+        header.classList.add("menu-movil");
+        btnLogin.classList.remove("btn-danger");
+        btnLogin.classList.add("btn-outline-light");
+
+    } else {
+        logo.setAttribute("src", "img/logo-bookmark.svg");
+        btnLogin.classList.remove("btn-outline-light");
+        btnLogin.classList.add("btn-danger");
+        header.classList.remove('menu-movil');
+    }
+}
 
 
